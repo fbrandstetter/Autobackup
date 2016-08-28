c6e41638d4322ff6cbd3ebe4d08640effc5cdb77
Florian <info@fbrandstetter.eu.org>
2016-08-28 14:55:26 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Backup Directory
+# Backup Directory ( where to store the encrypted backups )
 BACKUPDIR=/data/backups/
 
 # Current Time ( needed for backup file naming )
@@ -12,11 +12,37 @@ PASSWORD="PZEz5BaqhKG4s4Zm" # Replace with a self-generated password
 # SERVERLIST
 SERVERLIST=$(cat serverlist.template)
 
+# Backuped Data ( what data to backup )
+BACKUPDATA=/home/
+
+# Loop through all servers and do something
 for data in $SERVERLIST
 do
+        # Set variables
         SERVERNAME=$( echo $data | cut -f1 -d '|' )
         USERNAME=$( echo $data | cut -f2 -d '|' )
-        echo $SERVERNAME $USERNAME
+
+        # Create directory for the backup
+        mkdir $BACKUPTIME-$SERVERNAME
+
+        # Grab files from remote server
+        echo "get -r $BACKUPDATA $BACKUPTIME-$SERVERNAME/" | sftp $USERNAME@$SERVERNAME
+
+        # Create archive
+        tar cvf $BACKUPTIME-$SERVERNAME.tar -C $BACKUPTIME-$SERVERNAME/ .
+
+        # Remove folder
+        rm -rf $BACKUPTIME-$SERVERNAME/
+
+        # Encrypt archive
+        openssl aes-256-cbc -salt -in $BACKUPTIME-$SERVERNAME.tar -out $BACKUPTIME-$SERVERNAME.tar.aes -k $PASSWORD
+
+        # Remove unencrypted archive
+        rm $BACKUPTIME-$SERVERNAME.tar
+
+        # Move archive
+        mv $BACKUPTIME-$SERVERNAME.tar.aes $BACKUPDIR
+
 done
 
 exit 0
