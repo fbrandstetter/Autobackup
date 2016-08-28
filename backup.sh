a90d99be2ede58d688f7d65003b38e6d32eb5f5b
Florian <info@fbrandstetter.eu.org>
2016-08-28 14:21:16 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Backup Directory
-BACKUPDIR = /data/backups/
+BACKUPDIR=/data/backups/
 
 # Current Time ( needed for backup file naming )
-BACKUPTIME = $(date +"%Y-%m-%d_%H%M")
+BACKUPTIME=$(date +"%Y-%m-%d_%H%M")
 
 # Password / Key for encryption
-PASSWORD = "PZEz5BaqhKG4s4Zm" # Replace with a self-generated password
+PASSWORD="PZEz5BaqhKG4s4Zm" # Replace with a self-generated password
 
 # SERVERLIST
-SERVERLIST = $(cat serverlist.template)
+SERVERLIST=$(cat serverlist.template)
 
 exit 0
