9a3d1de0883eb16a5fd61ba0111f6d7340b7c9af
Florian <info@fbrandstetter.eu.org>
2016-08-28 14:34:05 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -12,4 +12,11 @@ PASSWORD="PZEz5BaqhKG4s4Zm" # Replace with a self-generated password
 # SERVERLIST
 SERVERLIST=$(cat serverlist.template)
 
+for data in $SERVERLIST
+do
+        SERVERNAME=$( echo $data | cut -f1 -d '|' )
+        USERNAME=$( echo $data | cut -f2 -d '|' )
+        echo $SERVERNAME $USERNAME
+done
+
 exit 0
